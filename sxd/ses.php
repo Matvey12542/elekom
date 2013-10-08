e3475eb213447b1bccd4fbdb370db43cb6a7b4aa
Kolya <Matvey12542@gmail.com>
2013-10-08 09:46:59 +0300

    install drupal

---
+++ sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '9d23482357aaa5ee526b7a0313244a78' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'elekom',
+      'my_pass' => 'elekom',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1381214588,
+    'lng' => 'ru',
+  ),
+);
+?>
